ebae382dc610235511a29a39c6ecda12f4374324
Steve Albonico <salbonico@gmail.com>
2019-01-18 00:39:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :words
+
+def initialize(keyword)
+  @keyword = keyword.split("").sort
+end
+
+def match(words)
+  output = []
+
+  index = 0
+  words.each do |word|
+  if word.split("").sort == @keyword
+    output.push(word)
+  end
+end
+return output
+end
+
+
+end
